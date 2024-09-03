2f3bdcfa68fdac47392188ff3ab81f81131f3dbe
ajith.woodenclouds <ajith.woodenclouds@gmail.com>
2024-09-04 03:17:09 +0530

    fix-the path issue

--- src/routing/AnimRoutes.js
+++ src/routing/AnimRoutes.js
@@ -1,6 +1,5 @@
 import React from "react";
-// import routes route & useLocation hook
-import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
+import { Routes, Route, useLocation, useNavigate, Navigate } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 // components
 import About from "../components/screens/About";
@@ -11,19 +10,18 @@ import Qualifications from "../components/screens/Qualifications";
 
 function AnimRoutes() {
     const location = useLocation();
-    const Navigate = useNavigate();
+    const navigate = useNavigate();
+    
     return (
         <AnimatePresence initial={true} mode="wait">
             <Routes key={location.pathname} location={location}>
-                <Route
-                    path="/"
-                    element={<Navigate to="/portfolio-website" replace />}
-                />
+                <Route path="/" element={<Navigate to="/portfolio-website" replace />} />
                 <Route path="/portfolio-website" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/works" element={<Projects />} />
                 <Route path="/skills" element={<Qualifications />} />
                 <Route path="/contact" element={<Contacts />} />
+                <Route path="*" element={<Navigate to="/portfolio-website" replace />} /> {/* Catch-all route */}
             </Routes>
         </AnimatePresence>
     );
